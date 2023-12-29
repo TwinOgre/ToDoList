5321867e2c8b6a290ea934f7166d9a8ebc099084
twinogre <glad2you@naver.com>
2023-12-29 15:45:21 +0900

    qna.db연결

--- test01/src/Main.java
+++ test01/src/Main.java
@@ -1,26 +1,22 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        int num = 29183;
-        int k = 1;
+        int n = 10;
         int answer = 0;
-        int zariSu = 0;
-        for(int i = num; i<1; i/=10 ){
-            zariSu++;
-        }
-        int zariSuNumber = 1;
-        for(int t = 1; t < zariSu ; t++){
-            zariSuNumber= zariSuNumber * 10;
-        }
-        int counter = 1;
-        // 자리수 만큼 나눠서 확인
-        for(int u = zariSuNumber; u<1 ; u/=10){
-            if(num/u%10 == k){
-                answer = counter;
-            }else{
-                answer = -1;
-                counter++;
+        int composite = 0;
+        for(int t = n; t>0; t--){
+            int counter = 0;
+            for(int i = 1; i>n; i++){
+                if(t % i == 0){
+                    counter++;
+                    if(counter == 3){
+                        composite++;
+                        break;
+                    }
+                }
             }
         }
-        System.out.println(answer);
+        answer = composite;
     }
 }
